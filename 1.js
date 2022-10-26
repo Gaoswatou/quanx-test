e22f970994e8ea1df7c6f5764f42f7812dd4e81b
Gaoswatou <719617315@qq.com>
2022-10-26 22:23:29 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,9 +1,32 @@
-/**
-[rewrite_local]
-^https:\/\/ssb\.service\.888iq\.cn\/ssb-api\/api\/agent\/getUpgradeConfig url script-response-body 自己命名.js
-[mitm] 
-hostname = ssb.service.888iq.cn
-*/
+// const url = "https://example.com/";
+// const method = "POST";
+// const headers = {"Field": "test-header-param"};
+// const data = {"info": "abc"};
+
+// const myRequest = {
+//     url: url,
+//     method: method, // Optional, default GET.
+//     headers: headers, // Optional.
+//     body: JSON.stringify(data) // Optional.
+// };
+
+// $task.fetch(myRequest).then(response => {
+//     // response.statusCode, response.headers, response.body
+//     console.log(response.body);
+//     $notify("Title", "Subtitle", response.body); // Success!
+//     $done();
+// }, reason => {
+//     // reason.error
+//     $notify("Title", "Subtitle", reason.error); // Error!
+//     $done();
+// });
+
+
+// [rewrite_local]
+// ^https:\/\/ssb\.service\.888iq\.cn\/ssb-api\/api\/agent\/getUpgradeConfig url script-response-body 自己命名.js
+// [mitm] 
+// hostname = ssb.service.888iq.cn
+
 
 let body = $response.body;
 let url = $request.url;
@@ -12,12 +35,12 @@ let obj = JSON.parse(body);
 cosnt agentConfigUrl='https://ssb.service.888iq.cn/ssb-api/api/agent/getUpgradeConfig'
 
 if(url.includes(agentConfigUrl)){
-  let data=obj.data
-  data.forEach(el=>{
+  obj.data.forEach(el=>{
     if(el.type===2){
       el.price=6688 
     }
   })
+  $notify("测试", "test", obj)
   body=JSON.stringify(obj)
 }
 $done({body})
